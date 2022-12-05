62701b40062f7801b2025258ca2f8c40b20e1961
Henry Hirshland <hhirshland@gmail.com>
2022-12-05 14:12:46 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          <b>SmartQ SMS Bot</b>
+        </h1>
+        <p>Your AI powered virtual assistant, available via sms.</p>
+        <a
+          className="App-link"
+          href="https://buy.stripe.com/3cs5kV9uW39N1s4288"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sign up
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
